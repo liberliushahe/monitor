4794f4ed772ed8acb50018605823a8cf50a71ba9
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-06-11 11:54:08 +0800

    优化

---
+++ src/main/java/com/monitor/utils/SheetUtil.java
@@ -0,0 +1,26 @@
+package com.monitor.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * 项目工具类
+ * @author liushahe
+ *
+ */
+public class SheetUtil {
+	/**
+	 * 获取用户编号
+     * 格式 U+年月日时分秒+6位随机数
+	 * @return
+	 */
+	public static String getUid() {
+		Date date=new Date();
+		SimpleDateFormat format=new SimpleDateFormat("yyyyMMddhhmmss");
+		String time=format.format(date);
+		String random=String.valueOf(new Random().nextInt(1000000));
+		return "u"+time+random;
+	}
+
+}
